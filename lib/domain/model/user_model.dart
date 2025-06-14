c9c6caae1b8dd67d710fea9c0150f7baf4c0f3cd
yeabsera <yeab.dev@gmail.com>
2025-06-14 15:03:44 +0300

    feat: add a user model

---
+++ lib/domain/model/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final String id;
+  final String email;
+  final String username;
+  final String password;
+
+  const UserModel({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.password,
+  });
+}
