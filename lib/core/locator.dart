028342c28b90ecb7dbbb85e0bab10f08e76c968b
yeabsera <yeab.dev@gmail.com>
2025-06-16 15:15:10 +0300

    ref: align structure with clean arch principles

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
-import 'package:grindly/data/repository/auth_repository.dart';
+import 'package:grindly/features/auth/data/repository/auth_repository.dart';
 
 final GetIt getIt = GetIt.instance;
 
--- lib/features/auth/domain/usecases/sign_up.dart
+++ lib/features/auth/domain/usecases/sign_up.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:grindly/data/repository/auth_repository.dart';
+import 'package:grindly/features/auth/data/repository/auth_repository.dart';
 
 class SignUp {
   final AuthRepository authRepo;
