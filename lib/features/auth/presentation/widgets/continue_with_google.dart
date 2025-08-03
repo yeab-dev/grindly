2ba56bf89e803134c22d468f452741508b06b9c3
yeab <yeab.dev@gmail.com>
2025-08-03 22:45:24 +0300

    feat: add google sign in feature

--- lib/features/auth/presentation/widgets/continue_with_google.dart
+++ lib/features/auth/presentation/widgets/continue_with_google.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grindly/features/auth/presentation/cubits/sign_in/sign_in_cubit.dart';
 
 class ContinueWithGoogle extends StatelessWidget {
   const ContinueWithGoogle({super.key});
@@ -7,51 +9,57 @@ class ContinueWithGoogle extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
     final width = MediaQuery.sizeOf(context).width;
-    return Center(
-      child: Column(
-        children: [
-          Text(
-            'Or continue with',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.secondary,
-              fontSize: 18,
-            ),
-          ),
-          SizedBox(height: height * 0.01),
-          IconButton(
-            color: Theme.of(context).colorScheme.primary,
-            icon: Container(
-              height: height * 0.045,
-              width: width * 0.25,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  width: 1.2,
+    return BlocBuilder<SignInCubit, SignInState>(
+      builder: (context, state) {
+        return Center(
+          child: Column(
+            children: [
+              Text(
+                'Or continue with',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 18,
                 ),
-                borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: height * 0.025,
-                    child: Image.asset("assets/images/google.png"),
-                  ),
-                  Text(
-                    'oogle',
-                    style: TextStyle(
+              SizedBox(height: height * 0.01),
+              IconButton(
+                color: Theme.of(context).colorScheme.primary,
+                icon: Container(
+                  height: height * 0.045,
+                  width: width * 0.25,
+                  decoration: BoxDecoration(
+                    border: Border.all(
                       color: Theme.of(context).colorScheme.tertiary,
-                      fontSize: 20,
+                      width: 1.2,
                     ),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: height * 0.025,
+                        child: Image.asset("assets/images/google.png"),
+                      ),
+                      Text(
+                        'oogle',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.tertiary,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                tooltip: "continue with Google",
+                onPressed: () {
+                  context.read<SignInCubit>().continueWithGoogle();
+                },
               ),
-            ),
-            tooltip: "continue with Google",
-            onPressed: () {},
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
