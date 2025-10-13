c276398e4ad7f78bff60f116f57d51f3e10363ca
yeab <yeab.dev@gmail.com>
2025-10-13 09:43:08 +0300

    feat: add a wakatime auth token entity - also set up the android manifest to support deeplinking

---
+++ lib/features/wakatime/wakatime-auth/domain/entities/wakatime_auth_token.dart
@@ -0,0 +1,9 @@
+class WakatimeAuthToken {
+  final String accessToken;
+  final String refreshToken;
+
+  const WakatimeAuthToken({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+}
