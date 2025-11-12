7dce314b48cd0fbfbf8dbb166285c01302a3124f
yeab <yeab.dev@gmail.com>
2025-11-12 09:42:06 +0300

    feat: add an abstract repo for grindly leaders

---
+++ lib/features/wakatime/wakatime_leaderboard/domain/repositories/grindly_leaders_repository.dart
@@ -0,0 +1,5 @@
+import 'package:grindly/features/wakatime/wakatime_leaderboard/domain/entities/grindly_leader.dart';
+
+abstract class GrindlyLeadersRepository {
+  Future<List<GrindlyLeader>> getGrindlyLeaders();
+}
