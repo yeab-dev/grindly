31340f9fad260c9a90c02d353baa3428d3f5878f
yeab <yeab.dev@gmail.com>
2025-11-12 10:21:20 +0300

    feat: add grindly leaders repo implementation

--- lib/features/wakatime/wakatime_leaderboard/domain/repositories/grindly_leaders_repository.dart
+++ lib/features/wakatime/wakatime_leaderboard/domain/repositories/grindly_leaders_repository.dart
@@ -1,6 +1,7 @@
+import 'package:grindly/features/wakatime/wakatime_leaderboard/data/models/grindly_leader_model.dart';
 import 'package:grindly/features/wakatime/wakatime_leaderboard/domain/entities/grindly_leader.dart';
 
 abstract class GrindlyLeadersRepository {
   Future<List<GrindlyLeader>> getGrindlyLeaders();
-  Future<void> saveGrindlyLeader({required GrindlyLeader leader});
+  Future<void> saveGrindlyLeader({required GrindlyLeaderModel leaderModel});
 }
